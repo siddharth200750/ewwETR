88ad0604d98f0684cfa1ed9320c25f98f079d163
siddharth200750 <73896869+siddharth200750@users.noreply.github.com>
2021-05-28 14:54:34 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -11,7 +11,7 @@ var world,boy;
 var launchingForce=100;
 
 function preload(){
-	boy=loadImage("images/boy.png");
+	boy=loadImage("boy.png");
   }
 
 function setup() {
@@ -116,7 +116,7 @@ function keyPressed() {
 	if(collision.collided){
 		console.log("collided");
 		Matter.Body.setStatic(lmango,false);	
-	}*/
+	*/}
   mangoBodyPosition=lmango.body.position
   stoneBodyPosition=lstone.body.position
   
